ebe707beb367309819e7d1b690fd68230ffd62d3
rbhriegu459 <rbhriegu459@gmail.com>
2023-12-26 20:13:49 +0530

    loginApi

--- app.js
+++ app.js
@@ -18,16 +18,26 @@ app.get('/signup', (req,res) =>{
 });
 
 app.post('/signup', (req,res) => {
-    const { username,name, password } = req.body;
-    const query = 'INSERT INTO users (username, name, password) VALUES (?, ?, ?)';
-        db.query(query, [username,name, password], (err, result) => {
-            if (err) {
-                console.error('MySQL query error:', err);
-                return res.status(500).sendFile(path.join(__dirname, '/public/error.html'));
-            }
+    const { email,name, password } = req.body;
 
-            console.log('User registered successfully');
-            res.sendFile(path.join(__dirname, 'public/expensePage.html'));
+        const query = 'SELECT * FROM users WHERE email = ?';
+        db.query(query, email, (err, results) => {
+          if (err) throw err;
+      
+          if (results.length > 0) {
+            res.redirect('/login');
+          } else {
+            const q = 'INSERT INTO users (email, name, password) VALUES (?, ?, ?)';
+            db.query(q, [email,name, password], (err, result) => {
+                if (err) {
+                    console.error('MySQL query error:', err);
+                    return res.status(500).sendFile(path.join(__dirname, '/public/error.html'));
+                }
+
+                console.log('User registered successfully');
+                res.sendFile(path.join(__dirname, 'public/expensePage.html'));
+            });
+          }
         });
 });
 
@@ -36,19 +46,19 @@ app.get('/login', (req,res) =>{
 });
 
 app.post('/login', (req, res) => {
-    const username = req.body.username;
+    const email = req.body.email;
     const password = req.body.password;
   
     // Check credentials in the database
-    const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
-    db.query(query, [username, password], (err, results) => {
+    const query = 'SELECT * FROM users WHERE email = ? AND password = ?';
+    db.query(query, [email, password], (err, results) => {
       if (err) throw err;
   
       if (results.length > 0) {
         res.sendFile(path.join(__dirname, 'public/expensePage.html'));
       } else {
         res.send(`
-        <h1>Invalid Username or password</h1>
+        <h1>Invalid email or password</h1>
         <a href="/login">Login</a>
         `);
       }
