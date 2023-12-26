f513cf5a12b090258018a06c81cd318e13c7053a
rbhriegu459 <rbhriegu459@gmail.com>
2023-12-26 16:25:35 +0530

    signupAPI

--- app.js
+++ app.js
@@ -19,18 +19,16 @@ app.get('/signup', (req,res) =>{
 
 app.post('/signup', (req,res) => {
     const { username,name, password } = req.body;
-
-    // Insert user into the database
     const query = 'INSERT INTO users (username, name, password) VALUES (?, ?, ?)';
-    db.query(query, [username,name, password], (err, result) => {
-        if (err) {
-            console.error('MySQL query error:', err);
-            return res.status(500).sendFile(path.join(__dirname, '/public/error.html'));
-        }
+        db.query(query, [username,name, password], (err, result) => {
+            if (err) {
+                console.error('MySQL query error:', err);
+                return res.status(500).sendFile(path.join(__dirname, '/public/error.html'));
+            }
 
-        console.log('User registered successfully');
-        res.send('User registered successfully');
-    });
+            console.log('User registered successfully');
+            res.send('User registered successfully');
+        });
 });
 
 app.get('/login', (req,res) =>{
